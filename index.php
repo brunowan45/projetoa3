19dbc693e252da8bf88318ec811b95f449af0acc
brunowan45 <46167256+brunowan45@users.noreply.github.com>
2023-05-30 11:21:05 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 session_start();
-echo $_SESSION['nome'];
-
 ?>
+
+<html>
+  <body>
+    olá, <?php
+    if(isset($_SESSION['nome'])==null){
+    ?>
+     visitante <br>
+     <a href="login.php">login</a>
+     <?php } else {
+        echo $_SESSION['nome']; ?>
+     <br><a href="cadastro.php">cadastrar</a><br>
+         <a href="logout.php">sair</a>
+         <?php } ?>
+   <body>
+</html>
