632c0f779052b3a6b37956be9f15b39980aa1775
brunowan45 <46167256+brunowan45@users.noreply.github.com>
2023-06-01 11:12:20 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <center>
+        <h1>Cadastro</h1>
+        <form id="Cadastro" action="cadastrar.php" method="POST">
+            nome: <input type = "text" name= "nome" required><br>
+            login: <input type = "text" name= "login" required><br>
+            senha: <input type = "password" name= "senha" required><br><br>
+            <input type = "submit" name = "cadastrar" value = "cadastrar">
+    </form>
+        </center>
+  </body>
+<html>
