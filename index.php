55a049fc02331e21fde32208f168ce64c6c06eed
brunowan45 <46167256+brunowan45@users.noreply.github.com>
2023-06-03 17:21:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
 <html>
   <body>
     <center>
-    <h1>olá, <?php
+    <h1>Olá, <?php
     if(isset($_SESSION['nome'])==null){
     ?>
      visitante </h1>
@@ -13,7 +13,8 @@ session_start();
      <?php } else {
         echo $_SESSION['nome']; ?>
      </h1><br><a href="cadastro.php">cadastrar</a><br>
-         <a href="alterar senha.php">alterar senhar</a><br>
+         <a href="listarusuario.php">lista usuário</a><br>
+         <a href="alterarsenha.php">alterar senha</a><br>
          <a href="logout.php">sair</a>
          <?php } ?>
      </center>
