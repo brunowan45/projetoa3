7dac94f2ddc557456abe5d687e914ff7cb3688b7
brunowan45 <46167256+brunowan45@users.noreply.github.com>
2023-06-05 09:10:53 -0300

    Update alterarsenha.php

--- alterarsenha.php
+++ alterarsenha.php
@@ -1,5 +1,5 @@
 <?php
-// Estabelecer conexão com o banco de dados
+
 $host = 'localhost';
 $dbname = 'bancoa3';
 $username = 'seu_usuario';
@@ -7,22 +7,22 @@ $password = 'sua_senha';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-// Verificar se o formulário foi enviado
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Obter os valores do formulário
+    
     $login = $_POST['login'];
     $currentSenha = $_POST['current_password'];
     $newSenha = $_POST['new_password'];
 
-    // Consulta para verificar o usuário e senha atual
+    
     $stmt = $conn->prepare('SELECT * FROM login WHERE login = :login AND senha = :senha');
     $stmt->bindParam(':login', $login);
     $stmt->bindParam(':senha', $currentSenha);
     $stmt->execute();
 
-    // Verificar se o usuário e senha atual estão corretos
+    
     if ($stmt->rowCount() > 0) {
-        // Atualizar a senha do usuário
+        
         $updateStmt = $conn->prepare('UPDATE login SET senha = :new_senha WHERE login = :login');
         $updateStmt->bindParam(':new_senha', $newSenha);
         $updateStmt->bindParam(':login', $login);
