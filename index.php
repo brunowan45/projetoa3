fa4746c8a3be1208794a78beca25aa58d8d24ca5
brunowan45 <46167256+brunowan45@users.noreply.github.com>
2023-06-06 11:06:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,21 +2,58 @@
 session_start();
 ?>
 
+<!DOCTYPE html>
 <html>
-  <body>
-    <center>
-    <h1>Olá, <?php
-    if(isset($_SESSION['nome'])==null){
-    ?>
-     Visitante </h1>
-     <a href="login.php">login</a>
-     <?php } else {
-        echo $_SESSION['nome']; ?>
-     </h1><br><a href="cadastro.php">Cadastrar</a><br>
-         <a href="listarusuario.php">Listas de usuários</a><br>
-         <a href="alterarsenha.php">Esqueceu a senha?</a><br>
-         <a href="logout.php">Sair</a>
-         <?php } ?>
-     </center>
-   <body>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Página Inicial</title>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			background-color: #f1f1f1;
+		}
+
+		center {
+			margin-top: 100px;
+		}
+
+		h1 {
+			color: #333;
+		}
+
+		a {
+			color: #999;
+			text-decoration: none;
+		}
+
+		a:hover {
+			text-decoration: underline;
+		}
+	</style>
+</head>
+<body>
+	<center>
+		<h1>Olá,
+			<?php
+			if (isset($_SESSION['nome']) == null) {
+				echo "Visitante";
+				?>
+		</h1>
+		<a href="login.php">Login</a>
+		<?php
+		} else {
+			echo $_SESSION['nome'];
+			?>
+		</h1>
+		<br>
+		<a href="cadastro.php">Cadastrar</a><br>
+		<a href="listarusuario.php">Lista de usuários</a><br>
+		<a href="alterarsenha.php">Esqueceu a senha?</a><br>
+		<a href="logout.php">Sair</a>
+		<?php
+		}
+		?>
+	</center>
+</body>
 </html>
