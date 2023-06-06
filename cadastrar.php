a1347c45c9ffb9d270a31b011d27558eb66261cd
brunowan45 <46167256+brunowan45@users.noreply.github.com>
2023-06-06 11:05:11 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -1,19 +1,43 @@
 <?php
-include ("conexao.php");
-$nome = isset($_POST['nome'] ) ? $_POST['nome'] : '';
-$login = isset($_POST['login'] ) ? $_POST['login'] : '';
+include("conexao.php");
+$nome = isset($_POST['nome']) ? $_POST['nome'] : '';
+$login = isset($_POST['login']) ? $_POST['login'] : '';
 $senha = isset($_POST['senha']) ? $_POST['senha'] : '';
 
-$insert = "insert into login (nome, login, senha)
-values
-('$nome', '$login', '$senha')";
+$insert = "INSERT INTO login (nome, login, senha)
+VALUES ('$nome', '$login', '$senha')";
 
-$query=mysqli_query($conexao, $insert);
+$query = mysqli_query($conexao, $insert);
 ?>
 
+<!DOCTYPE html>
 <html>
-    <body>
-        <h2>Cadastro Realizado com sucesso</h2>
-        <a href = "index.php">Voltar</a>
-    </body>
+<head>
+    <title>Cadastro Realizado</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f1f1f1;
+            text-align: center;
+            padding-top: 100px;
+        }
+
+        h2 {
+            color: #333;
+        }
+
+        a {
+            color: #999;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <h2>Cadastro Realizado com sucesso</h2>
+    <a href="index.php">Voltar</a>
+</body>
 </html>
